a506c36b19b02d90c6f83060c53f6004ab36a715
skanungo-chr <sonam.kanungo@chrsolutions.com>
2026-04-08 09:08:25 +0530

    Reduce SharePoint page size to 100 to avoid server timeout

--- my-next-app/lib/cip.ts
+++ my-next-app/lib/cip.ts
@@ -112,7 +112,7 @@ export async function fetchCIPRecordsPage(
   } else {
     const siteId = await getSiteId(token);
     const listId = await getListId(siteId, resolvedList, token);
-    url = `/sites/${siteId}/lists/${listId}/items?expand=fields(select=${FIELDS_SELECT})&$filter=fields/ContentType ne 'Folder'&$top=500`;
+    url = `/sites/${siteId}/lists/${listId}/items?expand=fields(select=${FIELDS_SELECT})&$filter=fields/ContentType ne 'Folder'&$top=100`;
   }
 
   const page = await graphFetch(url, token) as { value: SPItem[]; "@odata.nextLink"?: string };
